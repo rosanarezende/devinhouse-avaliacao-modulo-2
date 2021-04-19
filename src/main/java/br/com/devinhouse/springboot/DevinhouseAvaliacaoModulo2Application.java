7a4b9b063d3ce0025f6fd7e36e1caee8db9e4c94
Rosana Rezende <rezende_rosana@hotmail.com>
2021-04-19 20:01:43 -0300

    inicia projeto

---
+++ src/main/java/br/com/devinhouse/springboot/DevinhouseAvaliacaoModulo2Application.java
@@ -0,0 +1,13 @@
+package br.com.devinhouse.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinhouseAvaliacaoModulo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinhouseAvaliacaoModulo2Application.class, args);
+	}
+
+}
